870ff3725b7d58db443ae239f6ee3c43441c8df0
Ersi Isufi <ersi.isufi@hotmail.com>
2026-05-16 20:32:47 +0300

    Security cautions

--- src/databse.py
+++ src/databse.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 # Connection details - replace with your DBeaver credentials
 DB_TYPE = 'postgresql'
-DB_USER = 'postgres'     
+DB_USER = os.getenv('DB_USER')     
 DB_PASS = os.getenv('DB_PASS')
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'mydb'
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
 
 def get_db_connection():
     """Establishes a connection to the PostgreSQL database."""
